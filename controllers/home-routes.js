382192952f0a6f66299ca46e73c52865f60be29d
Ashton B <ashtonbrugh@gmail.com>
2022-01-12 11:07:21 -0500

    adding authentication

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
+    console.log(req.session);
+
   res.render('homepage', {
       id: 1,
       post_url: 'https://handlebarsjs.com/guide/',
@@ -12,6 +14,15 @@ router.get('/', (req, res) => {
           username: 'test_user'
       }
   });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
